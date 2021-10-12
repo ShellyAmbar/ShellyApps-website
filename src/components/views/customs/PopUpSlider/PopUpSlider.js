0731f9f8394de3d1ab19db690b0f0782adb24cef
shelly ambar <ambar.rachel@gmail.com>
2021-10-12 16:55:49 +0300

    adding technologies and bubbbles effect

--- src/components/views/customs/PopUpSlider/PopUpSlider.js
+++ src/components/views/customs/PopUpSlider/PopUpSlider.js
@@ -10,7 +10,7 @@ function PopUpSlider({ data, sliderTitle, handleClickCard }) {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
     cssEase: "linear",
   };
