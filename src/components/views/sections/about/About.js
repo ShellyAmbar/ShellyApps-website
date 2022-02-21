f1ded3a672b43d71e266b947281670d231b51e08
Shelly-Ambar <ambar.rachel@gmail.com>
2022-02-21 10:33:00 +0200

    fixing about animations

--- src/components/views/sections/about/About.js
+++ src/components/views/sections/about/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./About.css";
 import logo from "../../../../images/me.jpg";
 import ScrollAnimations from "../../customs/ScrollAnimations";
@@ -6,7 +6,7 @@ import ScrollAnimations from "../../customs/ScrollAnimations";
 const About = () => {
   useEffect(() => {
     ScrollAnimations();
-   }, []);
+  }, []);
   return (
     <div id="About" className="about-container">
       <div className="about-body">
@@ -14,25 +14,27 @@ const About = () => {
 
         <img alt="" src={logo} className="img-circular fade-in " />
 
-        <p className="slide-in">
-          Welcome! I'm Shelly Ambar, a Software developer from Ramat
-          Gan, Israel.
+        <p className="about-section fade-in">
+          Welcome! I'm Shelly Ambar, a Software developer from Ramat Gan,
+          Israel.
         </p>
-        <p className="slide-in">
+        <p className="about-section fade-in">
           I Graduated BS.c in Computer Science from HIT in 2019,
         </p>
-        <p className="slide-in">
-         and for the last 2 years I worked in the industry as a  mobile apps developer  (Android with Java and IOS with Swift)
+        <p className="about-section fade-in">
+          and for the last 2 years I worked in the industry as a mobile apps
+          developer (Android with Java and IOS with Swift)
         </p>
-       
-        <p className="slide-in">
-        and as a freelancer Fullstack developer with React - React Native - node JS.
+
+        <p className="about-section fade-in">
+          and as a freelancer Fullstack developer with React - React Native -
+          node JS.
         </p>
-        <p className="slide-in">
+        <p className="about-section fade-in">
           I have over 60 projects in my Github page with various types of
           technologies.
         </p>
-        <p className="slide-in">
+        <p className="about-section fade-in">
           I'm hard-working and always looking to learn new technologies.
         </p>
       </div>
