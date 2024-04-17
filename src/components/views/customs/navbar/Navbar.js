9a6e5aabca962d586da3937f9704d969851c8f2a
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-04-17 07:17:37 +0300

    updaing info

--- src/components/views/customs/navbar/Navbar.js
+++ src/components/views/customs/navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../../../../images/logoo3.png";
 import "./Navbar.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
--- src/components/views/sections/header/Header.js
+++ src/components/views/sections/header/Header.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from "react";
+import React from "react";
 import "./Header.css";
 
 import {Link} from "react-scroll";
